54ff9013f66838d374b172221c4ccf4c536aff2a
higuchi shuya <higuchi_s@mti.co.jp>
2020-07-21 11:49:34 +0900

    [mod] 最初のページにコンポーネントでリストを表示するところまで実行
    形はかなり不恰好

--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -1,20 +1,19 @@
 <template>
   <v-container>
-    <v-list-item
-      text
-      color="black"
-      :to="path"
-      style="text-decoration: none; padding-left: 0px;"
-    >
-      <v-list-item-content>
-        <v-list-item-title>
-          {{ title }}
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          {{ subtitle }}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in path"
+        :key="i"
+        text
+        color="black"
+        style="text-decoration: none; padding-left: 0px;"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ title[i] }}</v-list-item-title>
+          <v-list-item-subtitle>{{ subtitle[i] }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ResultList :path="['url']" :title="['title']" :subtitle="['sub']" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ResultList from "@/components/ResultList.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    ResultList
   }
-}
+};
 </script>
