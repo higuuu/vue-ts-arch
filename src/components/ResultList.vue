eb3e882cbd7fabf85d94f148001288ef3de523f3
higuchi shuya <higuchi_s@mti.co.jp>
2020-07-21 11:35:18 +0900

    [add] Result List 結果一覧表示の方法

---
+++ src/components/ResultList.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-list-item
+      text
+      color="black"
+      :to="path"
+      style="text-decoration: none; padding-left: 0px;"
+    >
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ title }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ subtitle }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class ResultList extends Vue {
+  @Prop() title!: string;
+  @Prop() subtitle!: string;
+  @Prop() path!: string;
+}
+</script>
