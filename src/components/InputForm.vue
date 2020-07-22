06f53e24c157a2780ed03c5960e227cf3d421c73
higuchi shuya <higuchi_s@mti.co.jp>
2020-07-22 17:01:17 +0900

    [mod] 入力フォームのサイズ調整とcss ファイルの追加

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,16 +1,30 @@
 <template>
-  <v-form>
-    <v-text-field
-      v-model="account"
-      :rules="accountRules"
-      label="Github account name"
-      required
-    ></v-text-field>
-  </v-form>
+  <v-card class="input-form-size">
+    <v-container>
+      <v-row>
+        <v-col cols="9">
+          <v-form>
+            <v-text-field
+              v-model="account"
+              :rules="accountRules"
+              label="Github account name"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-col>
+        <v-col cols="3">
+          <v-btn>
+            送信
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import "../assets/style.css";
 
 @Component
 export default class InputForm extends Vue {
