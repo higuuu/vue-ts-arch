18a76b1c47415c78ab9919337194a5bfa1ffa03a
higuchi shuya <higuchi_s@mti.co.jp>
2020-07-22 18:05:09 +0900

    [mod] バリデーションの修正とタイトル付与によるUIの向上

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,19 +1,23 @@
 <template>
   <v-card class="input-form-size">
+    <v-card-title>
+      Githubアカウント検索
+    </v-card-title>
     <v-container>
       <v-row>
         <v-col cols="9">
-          <v-form>
+          <v-form :lazy-validation="false">
             <v-text-field
               v-model="account"
               :rules="accountRules"
               label="Github account name"
+              outlined
               required
             ></v-text-field>
           </v-form>
         </v-col>
         <v-col cols="3">
-          <v-btn @click="submit(account)">
+          <v-btn @click="submit(account)" color="primary">
             送信
           </v-btn>
         </v-col>
@@ -29,7 +33,9 @@ import "../assets/style.css";
 @Component
 export default class InputForm extends Vue {
   private account?: string = "";
-  private accountRules = [() => !!this.account || "Account Name is required"];
+  private accountRules = [
+    (value?: string) => !!value || "Account Name is required"
+  ];
   @Emit("submit") submit(account?: string) {
     return account;
   }
