c238264692acf6a280fdb6a41d5e0d64bebbbdca
higuchi shuya <higuchi_s@mti.co.jp>
2020-08-24 16:11:52 +0900

    [mod] アラートが表示されないパターンもテスト

--- tests/unit/Result.spec.ts
+++ tests/unit/Result.spec.ts
@@ -17,3 +17,22 @@ describe("components/ResultList test", () => {
     expect(alert.exists()).toBe(true);
   });
 });
+
+describe("components/ResultList test", () => {
+  it("ResultListが空ならエラーメッセージが表示されない", () => {
+    const wrapper = mount(ResultList, {
+      localVue,
+      propsData: {
+        repos: [
+          {
+            name: "dummy_name1",
+            html_url: "dummy_url1",
+            description: "dummy_description1"
+          }
+        ]
+      }
+    });
+    const alert = wrapper.find("#alert_message");
+    expect(alert.exists()).toBe(false);
+  });
+});
